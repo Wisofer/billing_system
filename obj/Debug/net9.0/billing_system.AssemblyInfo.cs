9498a5f953eafa84f22af92b17e0cd88d08bce30
William <wisofer17@gmail.com>
2025-12-02 01:57:37 -0600

    mejorando servicios

--- obj/Debug/net9.0/billing_system.AssemblyInfo.cs
+++ obj/Debug/net9.0/billing_system.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("billing_system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7466b1c4d50737a78592e1050a7953f45e54a157")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a8c219e8021201884986a40429855fa09e49373")]
 [assembly: System.Reflection.AssemblyProductAttribute("billing_system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("billing_system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
