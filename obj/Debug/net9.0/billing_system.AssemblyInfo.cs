a6b83463a117e4b0f7ca013522f0d0e9903acda7
William <wisofer17@gmail.com>
2025-12-08 00:15:39 -0600

    cambiando el login

--- obj/Debug/net9.0/billing_system.AssemblyInfo.cs
+++ obj/Debug/net9.0/billing_system.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("billing_system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2d0fedb60cfed61c6c70b8e72880d0e9aabc48f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a58d6712a715399518a32b3aebc52b84ee3b8d02")]
 [assembly: System.Reflection.AssemblyProductAttribute("billing_system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("billing_system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
